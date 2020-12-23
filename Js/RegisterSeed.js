54e7a7e670bc29822b3ec49c88c6c5f1f0d0fd45
Judy <yijunglin79013@gmail.com>
2020-12-23 13:44:24 +0800

    creat 傳送表單

--- Js/RegisterSeed.js
+++ Js/RegisterSeed.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
+
+    // 將input值傳入modal欄位
     $("#formSubmit").click(function () {
         var longitudeValue = document.getElementById("longitude").value;
         var latitudeValue = document.getElementById("latitude").value;
@@ -7,5 +9,10 @@ $(document).ready(function(){
         document.getElementById("longitudeCheck").innerHTML = longitudeValue;
         document.getElementById("latitudeCheck").innerHTML = latitudeValue;
         document.getElementById("numberCheck").innerHTML = numberValue;
+    });
+
+    // 按下確認鈕後將 form submit 至後端
+    $("#confirm").click(function () {
+        $("#seedForm").submit();
     });
 });
