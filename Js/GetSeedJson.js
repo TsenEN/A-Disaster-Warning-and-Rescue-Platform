c6769b7939df933e88585f318fada6ae3ffbb770
Kathleen7474 <63449817+Kathleen7474@users.noreply.github.com>
2021-01-12 15:33:24 +0800

    Add files via upload
    顯示車輛欄位跟地圖上的車輛

--- Js/GetSeedJson.js
+++ Js/GetSeedJson.js
@@ -1,36 +1,72 @@
-$(document).ready(function() {
-    // Fetch the initial table
-    refreshTable();
+$(document).ready(function () {
+	// Fetch the initial table
+	refreshTable();
 });
 
 function refreshTable() {
-		$.ajax({ 
-			type: 'GET', 
-			url: 'http://140.116.245.229:3000/GetSeedsJson', 
-			dataType: 'json',
-			success: function (JData) {
-				var i = 0; 
-				var SeedsListData = '';
-				$.each(JData, function() {
-					SeedsListData += '<tr id="rowSeedsStatus" class="">';
-					SeedsListData += '<td>'+'北區'+'</td>';
-					SeedsListData += '<td>'+JData[i].seed_id +'</td>';
-					SeedsListData += '<td>'+JData[i].seed_battery+'</td>';
-					SeedsListData += '<td>'+JData[i].seed_status+'</td>';
-					SeedsListData += '<td>'+JData[i].seed_latitude+'</td>';
-					SeedsListData += '<td>'+JData[i].seed_longitude+'</td>';
-					SeedsListData += '</tr>'; 
+	$.ajax({
+		type: 'GET',
+		url: 'http://140.116.245.229:3000/GetSeedsJson',
+		dataType: 'json',
+		success: function (JData) {
+			var i = 0;
+			var SeedsListData = '';
+			$.each(JData, function () {
+				SeedsListData += '<tr id="rowSeedsStatus" class="">';
+				SeedsListData += '<td>' + '北區' + '</td>';
+				SeedsListData += '<td>' + JData[i].seed_id + '</td>';
+				SeedsListData += '<td>' + JData[i].seed_battery + '</td>';
+				SeedsListData += '<td>' + JData[i].seed_status + '</td>';
+				SeedsListData += '<td>' + JData[i].seed_latitude + '</td>';
+				SeedsListData += '<td>' + JData[i].seed_longitude + '</td>';
+				SeedsListData += '</tr>';
 
 				i++;
-				});
-				$('#SeedsList').html(SeedsListData);
-				setTimeout(refreshTable, 1000);
-				//Fetch every 1 seconds
-			},
-			
-			error:function(xhr){
-				alert("ERROR: " + xhr.status + " " + xhr.statusText);
-			},
+			});
+			$('#SeedsList').html(SeedsListData);
+			setTimeout(refreshTable, 1000);
+			//Fetch every 1 seconds
+		},
 
-		});
+		error: function (xhr) {
+			alert("ERROR: " + xhr.status + " " + xhr.statusText);
+		},
+
+	});
+	$.ajax({ //Get Car Json
+		type: 'GET',
+		url: 'http://140.116.245.229:3000/GetCarsJson',
+		dataType: 'json',
+		success: function (JData) {
+			var i = 0;
+			var CarsListData = '';
+			var CarStatString = '';
+			$.each(JData, function () {
+				if (JData[i].car_status == 1) {
+					CarStatString = "派遣中";
+				}
+				else {
+					CarStatString = "未派遣";
+				}
+				CarsListData += '<tr id="rowCarsStatus" class="">';
+				CarsListData += '<td>' + JData[i].car_license_plate + '</td>';
+				CarsListData += '<td>' + CarStatString + '</td>';
+				if (JData[i].car_status == 1) {
+					CarsListData += '<td><button id = "CarButton' + JData[i].car_license_plate + '" class="btn btn-primary  text-light disabled">派遣</button></td>';
+				}
+				else {
+					CarsListData += '<td><button id = "CarButton' + JData[i].car_license_plate + '" class="btn btn-primary text-light">派遣</button></td>';
+				}
+				CarsListData += '</tr>';
+				i++;
+			});
+			$('#CarsList').html(CarsListData);
+			setTimeout(refreshTable, 1000000);
+			//Fetch every 1 seconds
+		},
+
+		error: function (xhr) {
+			alert("ERROR: " + xhr.status + " " + xhr.statusText);
+		},
+	});
 }	
--- Js/GoogleMap.js
+++ Js/GoogleMap.js
@@ -1,48 +1,78 @@
-
+var car_latlng = new Map();
+var car_status = new Map();
+var car_markers = [];
 var locations = [];
-
+var map
 function initMap() {
-	$.ajax({ 
-		type: 'GET', 
-		url: 'http://140.116.245.229:3000/GetSeedsJson', 
+	$.ajax({
+		type: 'GET',
+		url: 'http://140.116.245.229:3000/GetSeedsJson',
 		dataType: 'json',
 		success: function (JData) {
-			var i = 0; 
-			$.each(JData, function() {
-				locations[i] = {lat: parseFloat(JData[i].seed_latitude), lng: parseFloat(JData[i].seed_longitude)};
+			var i = 0;
+			$.each(JData, function () {
+				locations[i] = { lat: parseFloat(JData[i].seed_latitude), lng: parseFloat(JData[i].seed_longitude) };
 				i++;
 			});
-			const map = new google.maps.Map(document.getElementById("map"), {
+			map = new google.maps.Map(document.getElementById("map"), {
 				zoom: 8,
 				center: { lat: 23.745523, lng: 120.912494 },
-			  });
-			  // Create an array of alphabetical characters used to label the markers.
-			  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-			  // Add some markers to the map.
-			  // Note: The code uses the JavaScript Array.prototype.map() method to
-			  // create an array of markers based on a given "locations" array.
-			  // The map() method here has nothing to do with the Google Maps API.
-			  const markers = locations.map((location, i) => {
+			});
+			// Create an array of alphabetical characters used to label the markers.
+			const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			// Add some markers to the map.
+			// Note: The code uses the JavaScript Array.prototype.map() method to
+			// create an array of markers based on a given "locations" array.
+			// The map() method here has nothing to do with the Google Maps API.
+			const markers = locations.map((location, i) => {
 				return new google.maps.Marker({
-				  position: location,
-				  label: labels[i % labels.length],
+					position: location,
+					label: labels[i % labels.length],
 				});
-			  });
-		  
-			  // Add a marker clusterer to manage the markers.
-			  new MarkerClusterer(map, markers, {
+			});
+
+			// Add a marker clusterer to manage the markers.
+			new MarkerClusterer(map, markers, {
 				imagePath:
-				  "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
-			  });	
+					"https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+			});
 		},
-		
-		error:function(xhr){
+
+		error: function (xhr) {
 			alert("ERROR: " + xhr.status + " " + xhr.statusText);
 		},
 
 	});
 
-  }
+}
+var interval = setInterval(function () {
+	/* do something that will execute every 3000 milliseconds*/
+	$.ajax({
+		url: "http://140.116.245.229:3000/GetCarsJson",
+		type: "POST",
+		dataType: "json",
+		success: function (JData) {
+			var NumOfJData = JData.length;
+			for (var i = 0; i < NumOfJData; i++) {
+				car_status.set(JData[i]["car_license_plate"], JData[i]["car_status"]);
+				car_latlng.set(JData[i]["car_license_plate"], { lat: JData[i]["car_latitude"], lng: JData[i]["car_longitude"] });
+				if (JData[i]["car_status"] == 0) {
+				}
+				else {
+					car_markers[i] = new google.maps.Marker({
+						position: car_latlng.get(JData[i]["car_license_plate"]),
+						icon: './Img/ambulance_s.png',
+						map: map,
+						label: JData[i]["car_license_plate"]
+					});
+				}
 
- 
- 
+
+			}
+		},
+		error: function () {
+			alert("ERROR!!!");
+		}
+	});
+}, 1000);
+
