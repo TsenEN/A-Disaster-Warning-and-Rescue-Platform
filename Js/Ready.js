4f1540600353942578e3bcefd1a694c415545c0c
Kathleen7474 <kathleen074763@gmail.com>
2021-03-26 02:23:50 +0800

    點選select bar會復原

--- Js/Ready.js
+++ Js/Ready.js
@@ -4,24 +4,26 @@ $(document).ready(function () {
 
   //Get firestation first
   GetFireStation();
+  let retun_tmp =
+    "<tr><td Align='Center' scope='col'>-先選擇消防隊-</Td><td Align='Center' scope='col'>-先選擇消防隊-</Td></tr>";
   //change select bar 1
   $(document).on('change', '#team_1', function () {
     $('#FireStationInfo').html('');
-    $('#CarsList').html('');
+    $('#CarsList').html(retun_tmp);
     $('#team_3').html('<option>-分隊-</option>');
     SelectTeam1(this.value);
   });
   //change select bar 2
   $(document).on('change', '#team_2', function () {
     $('#FireStationInfo').html('');
-    $('#CarsList').html('');
+    $('#CarsList').html(retun_tmp);
     var element = document.getElementById('team_1');
     SelectTeam2(element.value, this.value);
   });
   //change select bar 3
   $(document).on('change', '#team_3', function () {
     $('#FireStationInfo').html('');
-    $('#CarsList').html('');
+    $('#CarsList').html(retun_tmp);
     var element1 = document.getElementById('team_1');
     var element2 = document.getElementById('team_2');
     SelectTeam3(element1.value, element2.value, this.value);
