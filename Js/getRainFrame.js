fdd499da5ded971c621f37f6d35b9ecef1bec8de
Yi-Jung0430 <54521513+Yi-Jung0430@users.noreply.github.com>
2021-03-26 17:17:29 +0800

    串接雨量api

---
+++ Js/getRainFrame.js
@@ -0,0 +1,15 @@
+// Curl
+
+// curl -X GET "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-60F77B1A-3D8B-468E-9D93-3FD7846A88C5&format=JSON&elementName=HOUR_24" -H  "accept: application/json"
+
+// Request URL
+// https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-60F77B1A-3D8B-468E-9D93-3FD7846A88C5&format=JSON&elementName=HOUR_24
+
+// Response headers
+
+// access-control-allow-origin: * 
+//  connection: keep-alive 
+//  content-encoding: gzip 
+//  content-type: application/json;charset=utf-8 
+//  date: Fri,26 Mar 2021 09:12:33 GMT 
+//  transfer-encoding: chunked 
