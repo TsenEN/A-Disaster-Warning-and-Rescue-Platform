1b915680feec5ec9963d129f2daca8488018fa5f
Yi-Jung0430 <54521513+Yi-Jung0430@users.noreply.github.com>
2021-04-01 11:44:32 +0800

    串接資料api

--- Js/getRainFrame.js
+++ Js/getRainFrame.js
@@ -14,12 +14,26 @@
 //  date: Fri,26 Mar 2021 09:12:33 GMT 
 //  transfer-encoding: chunked 
 
-var $content = $('#content');
-$.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-60F77B1A-3D8B-468E-9D93-3FD7846A88C5&format=JSON&elementName=HOUR_24',function(e){
-  e.forEach(function(o){
-    if(o.County=='高雄市'){
-      console.log(o);
-      $content.append('< '+o.SiteName+' > PM2.5:'+o['PM2.5']+', PM10:'+o.PM10+'<br/>');
-    }
-  });
-});
+// var $content = $('#content');
+// $.get('https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-60F77B1A-3D8B-468E-9D93-3FD7846A88C5&format=JSON&elementName=HOUR_24',function(e){
+//   e.forEach(function(o){
+//     if(o.County=='高雄市'){
+//       console.log(o);
+//       $content.append('< '+o.SiteName+' > PM2.5:'+o['PM2.5']+', PM10:'+o.PM10+'<br/>');
+//     }
+//   });
+// });
+
+var data;
+$.ajax({
+  url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-60F77B1A-3D8B-468E-9D93-3FD7846A88C5&format=JSON",
+  method: "GET",
+  dataType: "json",
+  success: function (re, i) {
+    console.log(re); //先測試拿到了什麼
+    data=re;  //透過 chrome.f12.console 輸入 data 確認內容
+    data=re.records.location[i]; 
+    console.log("hi");
+    console.log(data);
+  }
+})
