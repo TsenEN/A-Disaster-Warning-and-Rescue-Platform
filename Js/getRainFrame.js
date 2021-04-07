f86b2a396c962e21e9a66276aa65878d7bbf46e0
Yi-Jung0430 <54521513+Yi-Jung0430@users.noreply.github.com>
2021-04-07 17:24:48 +0800

    撈取所需資料

--- Js/getRainFrame.js
+++ Js/getRainFrame.js
@@ -24,23 +24,62 @@
 //   });
 // });
 
-var data;
-var Kaohsiung = [];
+let data;
+let Kaohsiung = [];
+const list = document.querySelector('#card-area');
+let str = '';
+
 $.ajax({
   url: "https://opendata.cwb.gov.tw/api/v1/rest/datastore/O-A0002-001?Authorization=CWB-60F77B1A-3D8B-468E-9D93-3FD7846A88C5&format=JSON",
   method: "GET",
   dataType: "json",
   success: function (re, i) {
     console.log(re); 
-    data=re.records.location; ;
-    console.log(data); 
-    data.forEach(function (item) {
-        console.log(item.parameter.parameterValue);
-        // if (item.parameter.parameterValue == "高雄市"){
-        //   Kaohsiung.push(item);
-        //   console.log(Kaohsiung);
-        // };
-
-      });
+    data=re.records.location;
+    addData();
+    showData();
   }
-})
+});
+
+function addData(){
+  data.forEach(function (item) {
+    if (item.parameter[0].parameterValue == "高雄市"){
+      let obj = {};
+      obj.administration = item.parameter[2].parameterValue;
+      obj.locationName = item.locationName;
+      obj.tenMinute = item.weatherElement[2].elementValue;
+      obj.threeHour = item.weatherElement[3].elementValue;
+      obj.daily = item.weatherElement[6].elementValue;
+      Kaohsiung.push(obj);
+    };
+  });
+};
+
+function showData(){
+  Kaohsiung.forEach(function (item) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                       
+      str += `<div class="col-3">
+        <ul class="card list-group border-primary">
+          <li class="card-body list-group-item">
+            <h5 class="card-title">${item.locationName}</h5>
+            <p class="card-text">${item.administration}</p>
+          </li>
+          <li class="card-body list-group-item text-center">
+            <h5 class="card-title">10分鐘累積雨量</h5>
+            <p class="card-text">${item.tenMinute}</p>
+          </li>
+          <li class="card-body list-group-item text-center">
+            <h5 class="card-title">3小時累積雨量</h5>
+            <p class="card-text">${item.threeHour}</p>
+          </li>
+          <li class="card-body list-group-item text-center">
+            <h5 class="card-title">日累積雨量</h5>
+            <p class="card-text">${item.daily}</p>
+          </li>
+        </ul>
+      </div>`;
+  });
+  list.innerHTML = str;
+};
+
+
+
